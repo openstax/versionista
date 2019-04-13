bc2412730df13ec65697b89b15f51daf3f4306f3
Nathan Stitt <nathan@stitt.org>
2019-04-12 20:48:34 -0500

    test fetching latest version from repository

---
+++ repository_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+	"net/http"
+)
+
+func TestGetLatestRelease(t *testing.T) {
+	client, mux, _, teardown := setup()
+	repo := NewRepository("foo/bar", client)
+	defer teardown()
+	specifiedVersion := "1.1.42"
+
+	mux.HandleFunc("/repos/foo/bar/releases/latest", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		fmt.Fprintf(w, `{"id":1, "tag_name": "%s"}`, specifiedVersion)
+	})
+
+	parsedVersion := repo.latestRelease()
+	if parsedVersion.String() != specifiedVersion {
+		t.Errorf("Latest release is %s, wanted %s", parsedVersion.String(), specifiedVersion)
+	}
+}
